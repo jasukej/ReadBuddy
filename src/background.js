c6cd5c8b294f8d193ec4af3f3a3fa440cfeba470
Kezia <roselinakeziarijadi@gmail.com>
2024-03-09 13:37:33 -0800

    Updated directory

--- src/background.js
+++ src/background.js
@@ -17,3 +17,12 @@ chrome.runtime.onInstalled.addListener(() => {
   function setPageStyle() {
     chrome.tabs.sendMessage(tab.id, {action: "toggleDyslexiaFriendly"});
   }
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'toggleDyslexicMode') {
+        // Forward the message to the active tab's content script
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "toggleDyslexicMode", fontDetails: request.fontDetails});
+        });
+    }
+});
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,6 @@
 
 document.getElementById('toggleButton').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {action: 'toggleDyslexiaMode'});
+        chrome.tabs.sendMessage(tabs[0].id, {action: 'toggleDyslexicMode'});
     });
 });
